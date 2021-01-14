f65f26b5ffd54c745d667c917d8ddb9f5e99e9df
kanari1569 <67040275+kanari1569@users.noreply.github.com>
2021-01-14 14:31:41 +0900

    config

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var app = express();
-const port = 3000
+const port = 3000;
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://kanari:1234@cluster0.nudpp.mongodb.net/user?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('hello world');
 });
 
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+
+        return res.status(200).json({ success: true })
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
