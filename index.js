e618740469a948bd9e8d027d9a9b4c3344b62b9c
kanari1569 <67040275+kanari1569@users.noreply.github.com>
2021-01-18 15:47:58 +0900

    jwt token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { User } = require('./models/User');
 const config = require('./config/key');
@@ -11,6 +12,7 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -27,5 +29,42 @@ app.post('/register', (req, res) => {
         return res.status(200).json({ success: true })
     })
 })
+
+app.post('/login', (req, res) => {
+    console.log("body:", req.body)
+    // console.log('ping')
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        console.log(req.body.password)
+        // console.log('user', user)
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            // console.log('err',err)
+
+            // console.log('isMatch',isMatch)
+
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+            //비밀번호 까지 맞다면 토큰을 생성하기.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
